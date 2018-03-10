e40ccd94b8ae48f0b5b6d19d70d91ea9742a381e
arthurvsn <arthurvsn@gmail.com>
2018-03-10 12:40:53 -0300

    First commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use \App\User;
+use \App\Response\Response;
+
+class UserController extends Controller
+{
+    private $response;
+
+    public function __construct() 
+    {
+        $this->response = new Response();
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::get();
+
+        $this->response->setTypeS();
+        $this->response->setDataSet($users);
+        $this->response->setMessages("Sucess!");
+
+        return response()->json($this->response->toString());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $user = $request->all();
+
+        $user['password'] = bcrypt($user['password']);        
+        User::create($user);
+
+        $this->response->setTypeS();
+        $this->response->setDataSet($user);
+        $this->response->setMessages("Sucess!");
+        
+        return response()->json($this->response->toString());
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Response/Response.php
@@ -0,0 +1,90 @@
+<?php 
+namespace App\Response;
+
+class Response 
+{
+    private $messages;
+    private $type;
+    private $dataSet;
+
+    public function __construct() 
+    {    }
+
+    /**
+     * Function to converte a object with options
+     * @return object $data
+     */
+    public function toString()
+    {   
+        $data = [];    
+        $messages = $this->getMessages();
+
+        $data['message']['text'] = $messages;
+        $data['message']['type'] = $this->getType();
+        
+        if ($this->getDataset() != "")
+        {
+            $data['dataset'] = $this->getDataset();
+        }
+
+        return $data;        
+    }
+
+    /**
+     * Setter for options messages to response object
+     * @param string $data message
+     */
+    public function setMessages($data)
+    {
+        $this->messages = $data;
+    }
+
+    /**
+     * get for options messages to response object
+     * @return string $data message
+     */
+    public function getMessages()
+    {
+        return $this->messages;
+    }
+
+    /**
+     * Set type padronizados, N or S to response object
+     */
+    public function setTypeN()
+    {
+        $this->type = 'N';
+    }
+
+    public function setTypeS()
+    {
+        $this->type = 'S';
+    }
+
+    /**
+     * Method Get for options type to response object
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Methhod Set for data options with object content
+     * @param object $data 
+     */
+    public function setDataSet($data)
+    {
+        $this->dataSet = $data;
+    }
+
+    /**
+     * Methhod get for data options with object content
+     * @return object $dataSet
+     */
+    public function getDataset()
+    {
+        return $this->dataSet;
+    }
+}
+?>
